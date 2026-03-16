b34d3e7b0b2b259a962b6f2b68e60839476ce479
Ilyas-00 <akioui.ilyas@gmail.com>
2026-03-16 13:52:09 +0100

    add new Solution LeetCode 160 Design Parking System

---
+++ LeetCode/160- Design Parking System/solution.rs
@@ -0,0 +1,35 @@
+struct ParkingSystem {
+    big : i32, 
+    medium : i32, 
+    small : i32
+}
+
+impl ParkingSystem {
+
+    fn new(big: i32, medium: i32, small: i32) -> Self {
+       ParkingSystem{
+        big, medium, small
+       }
+    }
+    
+    fn add_car(&mut self, car_type: i32) -> bool {
+        if car_type == 1 {
+            if self.big > 0 {
+                self.big -= 1; 
+                return true;    
+            } else {return false}
+        } else if  car_type == 2 {
+            if self.medium > 0 {
+                self.medium -= 1; 
+                return true ; 
+            } else {return false}
+        } else if car_type == 3 {
+            if self.small > 0 {
+                self.small -= 1; 
+                return true; 
+            } else {return false}
+        } else {
+            return false;   
+        }
+    }
+}
